69bcf3453792f002511cdb98419ef2bd9515b51a
VaibhavAmeria <101673600+VaibhavAmeria@users.noreply.github.com>
2025-01-12 02:34:22 +0530

    Time: 14 ms (49.32%) | Memory: 54.6 MB (52.34%) - LeetSync

---
+++ 952-word-subsets/word-subsets.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    int[] getCharacterCount(String s) {
+        int[] freq = new int[26];
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+        return freq;
+    }
+
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        List<String> ls = new ArrayList<>();
+        int[] maxFreq = new int[26];
+        for (String word : words2) {
+            int[] currFreq = getCharacterCount(word);
+            for (int i = 0; i < 26; i++) {
+                maxFreq[i] = Math.max(maxFreq[i], currFreq[i]);
+            }
+        }
+
+        // Check each word in words1
+        for (String word : words1) {
+            int[] currWordFreq = getCharacterCount(word);
+            boolean isUniversal = true;
+
+            for (int i = 0; i < 26; i++) {
+                if (currWordFreq[i] < maxFreq[i]) {
+                    isUniversal = false;
+                    break;
+                }
+            }
+
+            if (isUniversal) {
+                ls.add(word);
+            }
+        }
+        
+        return ls;
+    }
+}
