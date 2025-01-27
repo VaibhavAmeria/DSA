540b15d90c19c491c3acc32e64ea4988c7c32540
VaibhavAmeria <101673600+VaibhavAmeria@users.noreply.github.com>
2025-01-28 00:34:24 +0530

    Time: 353 ms (13.23%) | Memory: 49.3 MB (28.09%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.java
+++ 1558-course-schedule-iv/course-schedule-iv.java
@@ -1,39 +1,42 @@
 class Solution {
-    boolean bfs (Map <Integer, List <Integer>> adj, int src, int dest, boolean [] visited) {
-        Queue <Integer> queue = new LinkedList <>();
-        queue.add(src);
-        visited[src] = true;
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-            if (curr == dest) {
-                return true;
-            }
-            for (int adjNode : adj.getOrDefault(curr, new ArrayList <>())) {
-                if (!visited[adjNode]) {
-                    visited[adjNode] = true;
-                    queue.add(adjNode);
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        for (int[] edge : prerequisites) {
+            adjList.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+        }
+        boolean[][] isPrerequisite = new boolean[numCourses][numCourses];
+        preprocess(numCourses, adjList, isPrerequisite);
+        List<Boolean> result = new ArrayList<>();
+        for (int[] query : queries) {
+            result.add(isPrerequisite[query[0]][query[1]]);
+        }
+
+        return result;
+    }
+    private void preprocess(int numCourses, Map<Integer, List<Integer>> adjList, boolean[][] isPrerequisite) {
+        for (int u = 0; u < numCourses; u++) {
+            for (int v = 0; v < numCourses; v++) {
+                if (u != v) {
+                    boolean[] visited = new boolean[numCourses];
+                    if (dfs(adjList, visited, u, v)) {
+                        isPrerequisite[u][v] = true;
+                    }
                 }
             }
         }
-        return false;
     }
-    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
-        Map <Integer, List <Integer>> adj = new HashMap <>();
-        for (int [] edge : prerequisites) {
-            int u = edge[0];
-            int v = edge[1];
-            adj.computeIfAbsent(u, k -> new ArrayList <>()).add(v);
+    private boolean dfs(Map<Integer, List<Integer>> adjList, boolean[] visited, int src, int target) {
+        visited[src] = true;
+
+        if (src == target) {
+            return true;
         }
 
-
-        List <Boolean> res = new ArrayList <>();
-        for (int [] query : queries) {
-            int src = query[0];
-            int dest = query[1];
-            boolean[] visited = new boolean[numCourses];
-            res.add(bfs(adj, src, dest, visited));
+        for (int adjNode : adjList.getOrDefault(src, new ArrayList<>())) {
+            if (!visited[adjNode] && dfs(adjList, visited, adjNode, target)) {
+                return true;
+            }
         }
-
-        return res;
+        return false;
     }
 }
