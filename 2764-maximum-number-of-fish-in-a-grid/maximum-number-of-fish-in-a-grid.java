72911ef005d81ec9fa765064353f39b2df1a1f51
VaibhavAmeria <101673600+VaibhavAmeria@users.noreply.github.com>
2025-01-29 00:33:08 +0530

    Time: 7 ms (24.93%) | Memory: 45.2 MB (43.70%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int findMaxFish(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        boolean[][] visited = new boolean[n][m];
+        int maxFish = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] != 0 && !visited[i][j]) {
+                    Queue <int []> queue = new LinkedList <>();
+                    visited[i][j] = true;
+                    queue.offer(new int [] {i, j});
+                    int currSum = 0;
+                    while (!queue.isEmpty()) {
+                        int size = queue.size();
+                        while (size-- > 0) {
+                            int [] curr = queue.poll();
+                            int x = curr[0];
+                            int y = curr[1];
+                            currSum += grid[x][y];
+                            for (int [] dir : directions) {
+                                int newX = x + dir[0];
+                                int newY = y + dir[1];
+                                if (newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] != 0 && !visited[newX][newY]) {
+                                    visited[newX][newY] = true;
+                                    queue.offer(new int [] {newX, newY});
+                                }
+                            }
+                            maxFish = Math.max(maxFish, currSum);
+                        }
+                    }
+                }
+            }
+        }
+        return maxFish;
+    }
+}
