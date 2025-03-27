5a62b1fa80be916ec4779c5b18d3136eac37ebd7
VaibhavAmeria <101673600+VaibhavAmeria@users.noreply.github.com>
2025-03-28 02:57:56 +0530

    Time: 30 ms (58.60%) | Memory: 62.4 MB (20.38%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -1,20 +1,39 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
-        Map <Integer, Integer> map1 = new HashMap <>();
-        Map <Integer, Integer> map2 = new HashMap <>();
         int n = nums.size();
-
+        Map <Integer, Integer> map = new HashMap <>();
         for (int num : nums) {
-            map2.put(num, map2.getOrDefault(num, 0) + 1);
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+
+        int majorityElement = -1;
+        int maxCount = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                majorityElement = entry.getKey();
+                maxCount = entry.getValue();
+            }
+        }
+
+        if (maxCount <= n / 2) {
+            return -1;
+        }
+
+        int currCount = 0;
         for (int i = 0; i < nums.size(); i++) {
             int num = nums.get(i);
-            map1.put(num, map1.getOrDefault(num, 0) + 1);
-            map2.put(num, map2.getOrDefault(num, 0) - 1);
+            // map1.put(num, map1.getOrDefault(num, 0) + 1);
+            // map2.put(num, map2.getOrDefault(num, 0) - 1);
+
+            if (num == majorityElement) {
+                currCount += 1;
+            }
 
             int n1 = i + 1;
             int n2 = n - i - 1;
-            if (map1.get(num) > n1/2 && map2.get(num) > n2/2) {
+            int leftCount = currCount;
+            int rightCount = maxCount - currCount;
+            if (leftCount > n1/2 &&  rightCount > n2/2) {
                 return i;
             }
         }
