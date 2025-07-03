41ca62d1371c0006a26aa5225e71c0925328f78b
VaibhavAmeria <101673600+VaibhavAmeria@users.noreply.github.com>
2025-07-04 00:50:46 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (99.85%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -1,16 +1,8 @@
 class Solution {
     public char kthCharacter(int k) {
         int characterIndex = k-1; // originally at k-1 index in final string
-
         // count no of setBits
-        int setBits = 0;
-        while (characterIndex != 0) {
-            if ((characterIndex & 1) == 1) {
-                setBits++;
-            }
-            characterIndex = characterIndex >> 1;
-        }
-        // a is the starting character
+        int setBits = Integer.bitCount(characterIndex);
         return (char) ('a' + setBits);
     }
 }
