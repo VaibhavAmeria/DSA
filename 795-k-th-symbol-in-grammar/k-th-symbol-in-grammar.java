b8ba4cff2be6914a09ee5ca09d55d88e9f9c7029
VaibhavAmeria <101673600+VaibhavAmeria@users.noreply.github.com>
2025-07-04 00:47:52 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (68.37%) - LeetSync

---
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int thueMorseRecursive(int n, int k) {
+        if (n == 1) {
+            return 0;
+        }
+        int halfLength = (int) Math.pow(2, n - 2); // Length of S(n-1)
+        if (k <= halfLength) {
+            return thueMorseRecursive(n - 1, k);
+        } else {
+            int c = thueMorseRecursive(n - 1, k - halfLength);
+            return (c == 0) ? 1 : 0;
+        }
+    }
+    public int kthGrammar(int n, int k) {
+        return thueMorseRecursive(n, k);
+    }
+}
+
