e9e3ba1df0797813a1622e6690b85b3e7ff7feee
VaibhavAmeria <101673600+VaibhavAmeria@users.noreply.github.com>
2025-09-30 10:04:21 +0530

    Time: 7 ms (99.11%) | Memory: 45.7 MB (5.31%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.java
@@ -6,12 +6,12 @@ class Solution {
             return 0;
         }
 
+        // find the largest triplet formed satisfying all conditons of triangle
         for (int i = n-1; i >= 2; i--) {
             if (nums[i-1] + nums[i-2] > nums[i]) {
                 return nums[i-1] + nums[i-2] + nums[i];
             }
         }
-
         return 0;
     }
 }
