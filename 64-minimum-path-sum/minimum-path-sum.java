2fa509cbc45e52784579178401517cd714fcd23e
VaibhavAmeria <101673600+VaibhavAmeria@users.noreply.github.com>
2026-01-01 18:24:08 +0530

    Time: 4 ms (62.72%) | Memory: 50.3 MB (50.91%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,26 +1,29 @@
 class Solution {
-    int [][] dp;
-    int n;
-    int m;
-    int soln (int i, int j, int [][] grid) {
-        if (i >= n || j >= m) {
-            return Integer.MAX_VALUE;
-        }
-        if (i == n-1 && j == m-1) {
-            return grid[i][j];
-        }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        return dp[i][j] = grid[i][j] + Math.min(soln(i, j+1, grid), soln(i+1, j, grid));
-    }
     public int minPathSum(int[][] grid) {
-        n = grid.length;
-        m = grid[0].length;
-        dp = new int[n][m];
+        int n = grid.length;
+        int m = grid[0].length;
+        int [][] dp = new int [n][m];
         for (int [] arr : dp) {
             Arrays.fill(arr, -1);
         }
-        return soln(0, 0, grid);
+
+        dp[n-1][m-1] = grid[n-1][m-1];
+
+        // fill last row
+        for (int i = m-2; i >= 0; i--) {
+            dp[n-1][i] = grid[n-1][i] + dp[n-1][i+1];
+        }
+
+        // fill last col
+        for (int i = n-2; i >= 0; i--) {
+            dp[i][m-1] = grid[i][m-1] + dp[i+1][m-1];
+        }
+
+        for (int i = n-2; i >= 0; i--) {
+            for (int j = m-2; j >= 0; j--) {
+                dp[i][j] = grid[i][j] + Math.min(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 }
