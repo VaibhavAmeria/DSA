bba07b24c3d320d99409038c536d7e9899148879
VaibhavAmeria <101673600+VaibhavAmeria@users.noreply.github.com>
2026-07-07 16:10:50 +0530

    Time: 8 ms (14.40%) | Memory: 46.8 MB (11.33%) - LeetSync

---
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        // if same starting, covering max distance comes first
+        // else sorted based on starting index
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            }
+            return a[0] - b[0];
+        });
+
+        int n = intervals.length;
+        List<int[]> res = new ArrayList<>();
+        res.add(intervals[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (res.get(res.size()-1)[0] <= intervals[i][0] && res.get(res.size()-1)[1] >= intervals[i][1]) {
+                continue;
+            }
+            res.add(intervals[i]);
+        }
+
+        return res.size();   
+    }
+}
