fe51855194a215e8332a44fd91bc3037ac5f1347
VaibhavAmeria <101673600+VaibhavAmeria@users.noreply.github.com>
2026-08-13 16:54:09 +0530

    Time: 7 ms (18.01%) | Memory: 45.3 MB (21.13%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.java
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -5,24 +5,21 @@ class Solution {
         int score = 0;
         int maxScore = 0;
         int i = 0;
-        int j = n - 1;
-        
+        int j = n-1;
+
         while (i <= j) {
             if (power >= tokens[i]) {
-                // Play face-up
                 power -= tokens[i];
                 score++;
                 i++;
                 maxScore = Math.max(maxScore, score);
             }
             else if (score > 0) {
-                // Play face-down (only if we have score to spend)
                 power += tokens[j];
                 score--;
                 j--;
             }
             else {
-                // Cannot play either way
                 break;
             }
         }
